549f670fdb1096fbe517a398d2e2844992d2def3
gaoxiao <gaoxiao6331@163.com>
2025-12-14 17:39:25 +0800

    feat: configure Jest with TypeScript support encies

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+// jest.config.js
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig.json');
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>'],
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
+    prefix: '<rootDir>/',
+  }),
+  testMatch: [
+    '**/__tests__/**/*.+(ts|tsx|js)',
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+};
