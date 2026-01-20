7a8728d8b072ff94ea195753798e9c35d17dd82e
gaoxiao <gaoxiao6331@163.com>
2026-01-21 00:55:14 +0800

    chore: update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,30 @@
-// jest.config.js
 const { pathsToModuleNameMapper } = require('ts-jest');
-const { compilerOptions } = require('./tsconfig.json');
+const { compilerOptions } = require('./tsconfig.json'); // 确保引入了 tsconfig
 
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>'],
+  
+  roots: ['<rootDir>/src'],
+
+  collectCoverage: true,
+
   modulePaths: [compilerOptions.baseUrl],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
     prefix: '<rootDir>/',
   }),
+
   testMatch: [
-    '**/__tests__/**/*.+(ts|tsx|js)',
+    '<rootDir>/src/**/__tests__/**/*.+(test|spec).+(ts|tsx|js|jsx)',
   ],
+
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx,js,jsx}',
+    '!src/**/*.d.ts',
+    '!src/**/__tests__/**',
+    '!src/**/*.test.ts'
+  ],
+
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
