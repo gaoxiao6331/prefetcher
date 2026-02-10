6f06b7b00296427c62f043d7d40649caab19b773
gaoxiao <gaoxiao6331@163.com>
2026-02-11 00:44:46 +0800

    chore: add trailing slash to prevent redirection

--- script/res_gen.sh
+++ script/res_gen.sh
@@ -3,7 +3,7 @@ TIMESTAMP=$(date +"%m%d-%H:%M")
 curl -X POST http://localhost:3000/res_gen \
      -H "Content-Type: application/json" \
      -d "{
-          \"targetUrl\": \"https://gaoxiao6331.github.io/prefetcher-examples/b\",
+          \"targetUrl\": \"https://gaoxiao6331.github.io/prefetcher-examples/b/\",
           \"projectName\": \"examples\",
           \"targetFileName\": \"ex-res.js\",
           \"template\": \"window.prefetch_list= __content_placeholder__; // this is for /b $TIMESTAMP\"
